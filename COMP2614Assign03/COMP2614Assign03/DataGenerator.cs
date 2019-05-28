09849d2f05fdb1e48b0e63a3d93c3a0acac55d4d
Joel Benoit <joel.benoit@tsunamisolutions.com>
2019-05-27 20:39:53 -0700

    Got 1 invoice number

--- COMP2614Assign03/COMP2614Assign03/DataGenerator.cs
+++ COMP2614Assign03/COMP2614Assign03/DataGenerator.cs
@@ -16,18 +16,28 @@ namespace COMP2614Assign03
                 string line;
                 string readContents;
                 //int invoices;
+                int convertedInvoiceNumber;
                 int invoiceNumber = 0;
                 while ((line = reader.ReadLine()) != null)
                 {
                     readContents = reader.ReadToEnd();
-                    string[] dataSectionByLine = readContents.Split(':');
-                    
-                    invoiceNumber = Convert.ToInt32(dataSectionByLine[0]);
-                    Console.WriteLine(invoiceNumber);
-                            
-     
+                    String removedNewLines = readContents.Replace("\r", "");
+                    String removedLines = removedNewLines.Replace("\n", "");
+                    string[] getSectionByColon = removedLines.Split(':');
+
+
+                    //foreach (string readDataByLine in getSectionByLine)
+                    //{
+                    //   string[] dataSectionByColon = readDataByLine.Split(':');
+                    //}
+
+                    invoiceNumber = Convert.ToInt32(getSectionByColon[0]);
+
+
+
                 }
-                     reader.Close();  
+         
+                reader.Close();  
                 InvoiceCollection invoices = new InvoiceCollection();
                 invoices.Add(new Invoice { InvoiceNumber = invoiceNumber });
 
